1283530bb03ba2fb9fb65bcae2b22e179ea450c0
猴子 <a102009102009@gmail.com>
2022-08-01 17:16:28 +0800

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,13 +3,51 @@
     <BaseSection class="info">
       <h1>CloudTown 雲鎮</h1>
       <h3>Minecraft 伺服器</h3>
+      <span class="runtime_span" v-text="runtimeSpan"></span>
       <a class="join" href="https://discord.gg/9wbGuaMHKN">加入雲鎮</a>
     </BaseSection>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { useIntervalFn } from '@vueuse/core';
 import BaseSection from '@/components/home/BaseSection.vue';
+
+const runtimeSpan = ref('');
+const baseDate = new Date('2022-07-22T16:00:00.000Z');
+
+const secondsMs = 1e3;
+const minutesMs = secondsMs * 60;
+const hoursMs = minutesMs * 60;
+const daysMs = hoursMs * 24;
+
+useIntervalFn(
+  () => {
+    const diff = new Date().getTime() - baseDate.getTime();
+
+    const days = Math.floor(diff / daysMs);
+    const leave1 = diff % daysMs;
+    const leave2 = leave1 % hoursMs;
+
+    const timeLest = [
+      days,
+      Math.floor(leave1 / hoursMs),
+      Math.floor(leave2 / minutesMs),
+      Math.round((leave2 % minutesMs) / secondsMs),
+    ];
+
+    const timeStr = ['天', '小時', '分鐘', '秒'];
+
+    runtimeSpan.value = timeLest
+      .map((count, index) => {
+        return `${count.toString().padStart(2, '0')} ${timeStr[index]}`;
+      })
+      .join(' ');
+  },
+  200,
+  { immediateCallback: true }
+);
 </script>
 
 <style lang="scss" scoped>
@@ -54,6 +92,11 @@ import BaseSection from '@/components/home/BaseSection.vue';
   .join {
     font-weight: 900;
     text-shadow: 0 0 10px white;
+  }
+
+  .runtime_span {
+    margin-top: 30px;
+    font-size: 20pt;
   }
 
   .join {
