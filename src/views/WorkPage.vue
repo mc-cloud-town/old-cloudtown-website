73128587b257985feaec012813aceaf793aacbc1
猴子 <a102009102009@gmail.com>
2022-10-18 20:50:50 +0800

    feat work

--- src/views/WorkPage.vue
+++ src/views/WorkPage.vue
@@ -1,8 +1,14 @@
+<script lang="ts" setup></script>
+
 <template>
   <GeneralHead />
-  <div></div>
+  <iframe
+    src="https://hackmd.io/@mc-cloud-town/Hkjc4f3mj"
+    width="100%"
+    height="100%"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
 </template>
-
-<script lang="ts" setup></script>
 
 <style lang="scss" scoped></style>
